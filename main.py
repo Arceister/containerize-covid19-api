8574e486dae7738c919d0eb99f09168f68ee7830
Arceister <raynvalshe@gmail.com>
2022-01-09 20:12:45 +0700

    fix: root endpoint to show api as same as api contract

--- main.py
+++ main.py
@@ -10,7 +10,16 @@ response = requests.get(base_url + "update.json").json()
 def return_normal_data():
     return_dict = {
         "ok": True,
-        "data": response["update"]["total"],
+        "data": {
+            "total_positive": response["update"]["total"]["jumlah_positif"],
+            "total_recovered": response["update"]["total"]["jumlah_sembuh"],
+            "total_deaths": response["update"]["total"]["jumlah_meninggal"],
+            "total_active": response["update"]["total"]["jumlah_dirawat"],
+            "new_positive": response["update"]["penambahan"]["jumlah_positif"],
+            "new_recovered": response["update"]["penambahan"]["jumlah_sembuh"],
+            "new_deaths": response["update"]["penambahan"]["jumlah_meninggal"],
+            "new_active": response["update"]["penambahan"]["jumlah_dirawat"]
+        },
         "message": "success"
     }
     return return_dict
