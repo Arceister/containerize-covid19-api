93fc57b274d725e918523987b81bb47c31a9523c
Arceister <raynvalshe@gmail.com>
2022-01-10 00:59:38 +0700

    feat: added daily endpoint

--- main.py
+++ main.py
@@ -86,6 +86,16 @@ def return_monthly_list_object(since, upto) -> dict:
     }
 
 def return_monthly_list_object_with_year(since, upto, year) -> dict:
+    if since[:4] != upto[:4]:
+        return HTTPException(422, "the year between two parameters must be the same year!")
+    if str(since[:4]) != str(year) or str(upto[:4]) != str(year):
+        return HTTPException(422, "query parameter year must be same with path parameter year!")
+    if year == "2020":
+        since = since[:5] + "03"
+    if year != "2020":
+        since = year + since[4:]
+        upto = year + upto[4:]
+
     data_list_to_show = []
 
     current_month = datetime.datetime.strptime(since, "%Y.%m")
@@ -94,6 +104,22 @@ def return_monthly_list_object_with_year(since, upto, year) -> dict:
     while current_month <= target_upto_month:
         data_list_to_show.append(return_specific_data_response(str(current_month)[:7])["data"])
         current_month += relativedelta(months = 1)
+
+    return {
+        "ok": True,
+        "data": data_list_to_show,
+        "message": "success"
+    }
+
+def return_daily_list_object(since, upto) -> dict:
+    data_list_to_show = []
+
+    current_date = datetime.datetime.strptime(since, "%Y.%m.%d")
+    target_upto_date = datetime.datetime.strptime(upto, "%Y.%m.%d") if response["update"]["penambahan"]["tanggal"] == str(upto.replace(".", "-")) else datetime.datetime.strptime(upto, "%Y.%m.%d") - datetime.timedelta(days=1)
+
+    while current_date <= target_upto_date:
+        data_list_to_show.append(return_specific_data_response(str(current_date)[:10])["data"])
+        current_date += datetime.timedelta(days=1)
 
     return {
         "ok": True,
@@ -121,20 +147,15 @@ async def read_query(since: str = "2020.03", upto: str = str(datetime.datetime.n
 
 @app.get("/monthly/{year}")
 async def read_query_and_parameter(year: str, since: str = "2020.01", upto: str = "2020.12"):
-    if since[:4] != upto[:4]:
-        return HTTPException(422, "the year between two parameters must be the same year!")
-    if str(since[:4]) != str(year) or str(upto[:4]) != str(year):
-        return HTTPException(422, "query parameter year must be same with path parameter year!")
-    if year == "2020":
-        since = since[:5] + "03"
-    if year is not "2020":
-        since = year + since[4:]
-        upto = year + upto[4:]
     return return_monthly_list_object_with_year(since, upto, year)
 
 @app.get("/monthly/{year}/{month}")
 async def read_parameter(year: str, month: str):
     return return_specific_data_response((year + "." + month).replace(".", "-"))
+
+@app.get("/daily")
+async def read_query(since: str = "2020.03.02", upto: str = str(datetime.datetime.now())[:10].replace("-", ".")):
+    return return_daily_list_object(since, upto)
 
 @app.get("/daily/{year}/{month}/{date}")
 async def read_parameter(year: str, month: str, date: str):
