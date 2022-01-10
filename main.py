706650006d5f62564e34f6ebac6e46d0610a397e
Arceister <raynvalshe@gmail.com>
2022-01-10 18:56:33 +0700

    refactor: remove unused code comments on `main.py`

--- main.py
+++ main.py
@@ -27,48 +27,6 @@ def return_specific_data_response(date) -> dict:
     }
     return return_dict
 
-# def return_monthly_list_object(since, upto) -> dict:
-#     data_list_to_show = []
-
-#     current_month = datetime.datetime.strptime(since, "%Y.%m")
-#     target_upto_month = datetime.datetime.strptime(upto, "%Y.%m")
-
-#     while current_month <= target_upto_month:
-#         data_list_to_show.append(return_specific_data_response(str(current_month)[:7])["data"])
-#         current_month += relativedelta(months = 1)
-
-#     return {
-#         "ok": True,
-#         "data": data_list_to_show,
-#         "message": "success"
-#     }
-
-# def return_monthly_list_object_with_year(since, upto, year) -> dict:
-#     if since[:4] != upto[:4]:
-#         raise HTTPException(422, "the year between two parameters must be the same year!")
-#     if str(since[:4]) != str(year) or str(upto[:4]) != str(year):
-#         raise HTTPException(422, "query parameter year must be same with path parameter year!")
-#     if year == "2020":
-#         since = since[:5] + "03"
-#     if year != "2020":
-#         since = year + since[4:]
-#         upto = year + upto[4:]
-
-#     data_list_to_show = []
-
-#     current_month = datetime.datetime.strptime(since, "%Y.%m")
-#     target_upto_month = datetime.datetime.strptime(upto, "%Y.%m")
-
-#     while current_month <= target_upto_month:
-#         data_list_to_show.append(return_specific_data_response(str(current_month)[:7])["data"])
-#         current_month += relativedelta(months = 1)
-
-#     return {
-#         "ok": True,
-#         "data": data_list_to_show,
-#         "message": "success"
-#     }
-
 def return_daily_list_object(since, upto) -> dict:
     data_list_to_show = []
 
@@ -132,18 +90,6 @@ app = FastAPI()
 app.include_router(root.router)
 app.include_router(yearly_router.router)
 app.include_router(monthly_router.router)
-
-# @app.get("/monthly")
-# async def read_query(since: str = "2020.03", upto: str = str(datetime.datetime.now())[:7].replace("-", ".")):
-#     return return_monthly_list_object(since, upto)
-
-# @app.get("/monthly/{year}")
-# async def read_query_and_parameter(year: str, since: str = "2020.01", upto: str = "2020.12"):
-#     return return_monthly_list_object_with_year(since, upto, year)
-
-# @app.get("/monthly/{year}/{month}")
-# async def read_parameter(year: str, month: str):
-#     return return_specific_data_response((year + "." + month).replace(".", "-"))
 
 @app.get("/daily")
 async def read_query(since: str = "2020.03.02", upto: str = str(datetime.datetime.now())[:10].replace("-", ".")):
